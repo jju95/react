32e4c29a4a56e4b725fbf19ccab08574ff769a8d
heeju.kim@onlinepowers.com <heeju.kim@onlinepowers.com>
2024-05-03 17:26:00 +0900

    05-03

---
+++ src/component/Square.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Square.css"
+
+export default class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+            </button>
+        )
+    }
+}
+
+/** default의 유무에 따라 import 형식이 달라짐 */
